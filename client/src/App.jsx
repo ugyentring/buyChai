a1ef25ada1762584a375f5b5e0b687d46a971e31
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-14 11:37:03 +0600

    donw with the metamask connectiona nd hardhat configurations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { ethers } from "hardhat";
 import { useState, useEffect } from "react";
 
 const App = () => {
@@ -6,23 +7,43 @@ const App = () => {
     signer: null,
     contract: null,
   });
+  const [account, setAccount] = useState("not connected");
 
   useEffect(() => {
     const template = async () => {
+      
       const contractAddress = "";
       const contractABI = "";
+      try {
+        // Metamask to do transactions
+        const { ethereum } = window;
 
-      //metamask to do transactions
-      const { ethereum } = window;
+        const account = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(account);
 
-      const account = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        setState({ provider, signer, contract });
+      } catch (error) {
+        console.log(error);
+      }
     };
     template();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>{account}</p>
+    </div>
+  );
 };
 
 export default App;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
