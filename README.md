ab70689bb4677d32f8b078137932676691b86e87
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-14 11:49:22 +0600

    tested my own way

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A basic application learn how blockchain interacts with the react frontend.
+
+<!--
+0x8775bBC2592baC1db083dc1FCC6F18753C23Ff87
+-->
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,34 @@
 import { ethers } from "hardhat";
-import { useState, useEffect } from "react";
+import abi from "./contractJSON/chai.json";
 
 const App = () => {
-  const [state, setState] = useState({
-    provider: null,
-    signer: null,
-    contract: null,
-  });
-  const [account, setAccount] = useState("not connected");
+  const template = async () => {
+    const contractAddress = "0x8775bBC2592baC1db083dc1FCC6F18753C23Ff87";
+    const contractABI = abi.abi;
+    try {
+      // Metamask to do transactions
+      const { ethereum } = window;
 
-  useEffect(() => {
-    const template = async () => {
-      
-      const contractAddress = "";
-      const contractABI = "";
-      try {
-        // Metamask to do transactions
-        const { ethereum } = window;
+      const account = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
-        const account = await ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setAccount(account);
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
 
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-
-        const contract = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          signer
-        );
-        setState({ provider, signer, contract });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    template();
-  }, []);
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="App">
-      <p>{account}</p>
+      <button onClick={template}></button>
     </div>
   );
 };
